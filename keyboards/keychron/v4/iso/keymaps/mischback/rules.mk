6311c9a593b06ce6c781402a05bfb821237e993c
Mischback <dev@mischback.de>
2023-01-10 21:32:45 +0100

    Disable Mac-specific layers

---
+++ keyboards/keychron/v4/iso/keymaps/mischback/rules.mk
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-FileCopyrightText: 2023 Mischback <dev@mischback.de>
+# SPDX-FileType: SOURCE
+
+# The Keychron V4 features a DIP switch at the back that let the user switch
+# between Mac and Windows layouts. As I run Linux anyways, this switch is not
+# in use for me personally.
+#
+# While this feature is deactivated, the default selection of layers in
+# Keychron's default keymap is not working and the first layer will be applied
+# as the default to the keyboard.
+#
+# TODO: This might be used for something different...
+#       See https://github.com/qmk/qmk_firmware/blob/master/docs/feature_dip_switch.md
+#       THIS WILL ALSO ENABLE Keychron's default function for switching layers
+#       depending on the position of the DIP, but that function is meant to be
+#       overridden by keymap specific code (see ``keyboards/keychron/v4/v4.c``),
+DIP_SWITCH_ENABLE = no
