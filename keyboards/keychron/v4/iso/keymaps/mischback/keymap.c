73de8f30462296f438ab8235929ab59a834d5994
Mischback <dev@mischback.de>
2023-01-11 21:16:58 +0100

    Create required layers
    Beside the base layer, there is a dedicated *movement layer*, an
    *editing layer* and a *control layer*.

--- keyboards/keychron/v4/iso/keymaps/mischback/keymap.c
+++ keyboards/keychron/v4/iso/keymaps/mischback/keymap.c
@@ -11,50 +11,88 @@
 
 #include QMK_KEYBOARD_H
 
-// clang-format off
-
+/* Create common reference to the defined layers.
+ *
+ * Note: Order does matter here, as the layers are stacked above each other.
+ *       LAYER_BASE is the root or default layer.
+ */
 enum layers {
-    BASE,
-    _FN1,
-    _FN2
+    LAYER_BASE,
+    LAYER_MOVE,
+    LAYER_EDIT,
+    LAYER_CTRL
 };
 
-#define KC_TASK LGUI(KC_TAB)
-#define KC_FLXP LGUI(KC_E)
+/* Define custom keycodes.
+ *
+ * Actually these are just shortcuts to fit into the layer keymap without
+ * overly destroying the neat columns.
+ */
+#define CC_LMOVE MO(LAYER_MOVE)  // read: Custom Code Layer Move
+#define CC_LEDIT MO(LAYER_EDIT)  // read: Custom Code Layer Edit
+#define CC_LCTRL MO(LAYER_CTRL)  // read: Custom Code Layer Control
+
+// clang-format off
 
 /* The actual definition of the layers.
  *
- * ``BASE`` is just a slight modification of the original layer as provided by
- * Keychron's default keymap for the keyboard (originally named ``WIN_BASE``):
- *   - make CapsLock toggle the first function layer
+ * ``LAYER_BASE`` is just a slight modification of the original layer as
+ * provided by Keychron's default keymap for the keyboard (originally named
+ * ``WIN_BASE``):
+ *   - [CapsLock] toggles ``LAYER_MOVE``
+ *   - [fn] toggles ``LAYER_MOVE``
+ *   - [fn1] toggles ``LAYER_EDIT``
+ *   - [fn + fn1] toggles ``LAYER_CTRL``
  *
  * As the DIP switch is disabled, the Mac-specific layers were removed.
  *
- * TODO: Check all keycodes of ``_FN1``, evaluate and then remove them, if not
- *       required.
- * TODO: Use ``_FN1`` for the actual main function layer:
- *         - *VI like movement*: ``hjkl``
- *         - *Gamer movement*: ``wasf`` (Yeah, I know... Left WASD long behind)
+ * TODO: Use ``LAYER_MOVE`` for the movement function layer:
+ *         - ``KC_LEFT``, ``KC_RGHT``, ``KC_DOWN``, ``KC_UP``
+ *           - *VI like movement*: ``hjkl``
+ *           - *Gamer movement*: ``wasf`` (Yeah, I know... Crazy!)
+ *         - ``KC_HOME``, ``KC_END``, ``KC_PGUP``, ``KC_PGDN``
+ * TODO: Use ``LAYER_EDIT`` for basic editing operations
+ * TODO: Use ``LAYER_CTRL`` for F-keys and control commands, including keyboard
+ *       specific configuration keys (lighting, ...):
+ *         - ``NK_TOGG``: Toggles NKRO (check docs again!)
+ *
+ * Reference EMPTY LAYER
+
+    [LAYER_NAME] = LAYOUT_iso_62(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
  */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [BASE] = LAYOUT_iso_62(
-        KC_ESC,   KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
-        KC_TAB,   KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,
-        MO(_FN1), KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,
-        KC_LSFT,  KC_NUBS,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            KC_RSFT,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                            KC_SPC,                             KC_RALT,  MO(_FN1), MO(_FN2), KC_RCTL),
+    [LAYER_BASE] = LAYOUT_iso_62(
+        KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,
+        CC_LMOVE, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,
+        KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  CC_LMOVE, CC_LEDIT, KC_RCTL),
 
-    [_FN1] = LAYOUT_iso_62(
-        KC_GRV,  KC_BRID,  KC_BRIU, KC_TASK, KC_FLXP, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  RGB_MOD,
-        RGB_TOG, RGB_MOD,  RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, KC_APP,  KC_SCRL, KC_INS,   KC_PGUP,  KC_HOME,
-        _______, RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, KC_UP,   KC_PSCR, KC_PGDN,  KC_END,   _______,  _______,
-        _______, _______,  _______, _______, _______, _______, _______, NK_TOGG, KC_LEFT, KC_DOWN, KC_RIGHT, KC_DEL,             _______,
-        _______, _______,  _______,                            _______,                            _______,  _______,  _______,  _______),
+    [LAYER_MOVE] = LAYOUT_iso_62(
+        KC_GRV,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  CC_LCTRL, _______),
 
-    [_FN2] = LAYOUT_iso_62(
-        KC_TILD, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   RGB_MOD,
-        RGB_TOG, RGB_MOD,  RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______,  _______,  _______,
-        _______, RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______,  _______,  _______,  _______,
-        _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,            _______,
-        _______, _______,  _______,                            _______,                            _______,  _______,  _______,  _______)
+    [LAYER_EDIT] = LAYOUT_iso_62(
+        KC_TILD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  _______,  _______,                                _______,                                _______,  CC_LCTRL, _______,  _______),
+
+    [LAYER_CTRL] = LAYOUT_iso_62(
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   RGB_MOD,
+        RGB_TOG,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 };
+
+// clang-format on
